44db4ad8832b8a5219af16a149ea32649522590c
Ramzi <ramziah.git.recolor221@passmail.net>
2025-01-16 01:42:19 -0600

    specify port to access pocketbase

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ( {fetch} ) => {
-    let devicesReq = await fetch("http://raspberrypi.local/api/collections/devices/records");
+    let devicesReq = await fetch("http://raspberrypi.local:8090/api/collections/devices/records");
     let devices = await devicesReq.json();
     return { devices };
 };
--- src/routes/tempHumidity/+server.ts
+++ src/routes/tempHumidity/+server.ts
@@ -1,7 +1,7 @@
 import {error, json} from '@sveltejs/kit';
 
 export async function GET({ url }) {
-    let fetchUrl = new URL("http://raspberrypi.local/api/collections/tempandhumidity/records");
+    let fetchUrl = new URL("http://raspberrypi.local:8090/api/collections/tempandhumidity/records");
     fetchUrl.searchParams.set('filter', url.searchParams.get('filter')!);
     fetchUrl.searchParams.set('sort', "-created");
     fetchUrl.searchParams.set('perPage', '720');
