1939c375407b060d6e2a79993fccb12b497c3f2b
Ramzi <ramziah.git.recolor221@passmail.net>
2025-01-16 01:44:07 -0600

    chart visual overhaul

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
 <div class="bg-gray-600">
 	<div class="flex flex-col justify-between gap-4 p-10">
 		{#each data.devices.items as device}
-			<div class="flex-col bg-gray-300 shadow-xl shadow-gray-800 rounded">
-				<div class="text-xl font-sans underline pt-2 pl-2">{device.deviceName} {device.id}</div>
+			<div class="flex-col bg-black shadow-xl shadow-gray-800 rounded">
+				<div class="text-white text-xl font-sans underline pt-2 pl-2">{device.deviceName} {device.id}</div>
 				<div class="flex-1">
 					<Graph data={device.id}/>
 				</div>
