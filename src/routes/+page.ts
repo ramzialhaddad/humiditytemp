0c4cf34eb1b71622f73b248c410541afe3b0da76
Ramzi <ramziah.git.recolor221@passmail.net>
2025-01-21 00:51:49 -0600

    implemented sample data use

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,14 @@
 import type { PageLoad } from './$types';
+import {useSampleData} from '$lib';
 
 export const load: PageLoad = async ( {fetch} ) => {
-    let devicesReq = await fetch("http://raspberrypi.local:8090/api/collections/devices/records");
-    let devices = await devicesReq.json();
-    return { devices };
+    if(useSampleData){
+        let devices = await import('../sampleData/devices.json');
+        return {devices};
+    }else{
+        let devicesReq = await fetch("http://raspberrypi.local:8090/api/collections/devices/records");
+        let devices = await devicesReq.json();
+        return { devices };
+    }
+
 };
--- src/routes/tempHumidity/+server.ts
+++ src/routes/tempHumidity/+server.ts
@@ -1,12 +1,33 @@
 import {error, json} from '@sveltejs/kit';
+import {useSampleData} from '$lib';
 
 export async function GET({ url }) {
-    let fetchUrl = new URL("http://raspberrypi.local:8090/api/collections/tempandhumidity/records");
-    fetchUrl.searchParams.set('filter', url.searchParams.get('filter')!);
-    fetchUrl.searchParams.set('sort', "-created");
-    fetchUrl.searchParams.set('perPage', '720');
+    if(useSampleData){
+        let deviceId = (url.searchParams.get('filter')!).slice(9, -2);
+        let temps;
+        switch(deviceId){
+            case '9857153337916':
+                temps = await import('../../sampleData/device1.json');
+                break;
+            case '198282125291056':
+                temps = await import('../../sampleData/device2.json');
+                break;
+            case '154739819958156':
+                temps = await import('../../sampleData/device3.json');
+                break;
+            default:
+                temps = await import('../../sampleData/device1.json');
+                break;
+        }
+        return json(temps);
+    }else{
+        let fetchUrl = new URL("http://raspberrypi.local:8090/api/collections/tempandhumidity/records");
+        fetchUrl.searchParams.set('filter', url.searchParams.get('filter')!);
+        fetchUrl.searchParams.set('sort', "-created");
+        fetchUrl.searchParams.set('perPage', '720');
 
-    let tempReq = await fetch(fetchUrl);
-    let temps = await tempReq.json();
-    return json(temps);
+        let tempReq = await fetch(fetchUrl);
+        let temps = await tempReq.json();
+        return json(temps);
+    }
 }
