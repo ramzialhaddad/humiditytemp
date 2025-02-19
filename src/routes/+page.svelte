b29cacec1433247a85b8b4c459dd9e0cc4bcbf9b
Ramzi <ramziah.git.recolor221@passmail.net>
2025-02-19 14:57:05 -0600

    Slightly changed layout and added copy ID function, requires UI confirmation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,27 @@
 
     let { data }: { data: PageData } = $props();
 
+    async function copyID(id: string){
+        try{
+            await navigator.clipboard.writeText(id);
+		}catch (err){
+            console.error("Failed to copy: ", err);
+		}
+	}
 </script>
-<div class="bg-gray-600">
+<div>
 	<div class="flex flex-col justify-between gap-4 p-1">
 		{#each data.devices.items as device}
-			<div class="flex-col bg-black shadow-xl shadow-gray-800 rounded">
-				<div class="text-white text-center text-xl font-sans underline pt-2 pl-2">{device.deviceName} {device.id}</div>
+			<div class="flex flex-col bg-black shadow-xl shadow-gray-800 rounded">
+				<div class="w-fit mx-auto mt-2  rounded flex flex-col align-middle">
+					<div class="text-white text-center text-2xl font-sans">{device.deviceName}</div>
+					<button class="bg-blue-950 rounded text-white text-center text-base font-mono
+					 mx-auto w-fit"
+					onclick={()=>copyID(device.id)}>
+						{device.id}
+						<i class="fa-solid fa-copy"></i>
+					</button>
+				</div>
 				<div class="flex-1">
 					<Graph data={device.id}/>
 				</div>
